274b8d94cc13801df8fc0eb9d203d99630a94f99
howmuchadollarcost <muluyonas@yahoo.com>
2019-03-09 12:05:52 +0100

    Updates

---
+++ precache-manifest.7a2c6f8f6ba3880c315afe57e6770205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "166aebdeb307c4ddf3f4",
+    "url": "/random-kanye-quotes/static/css/main.58611a83.chunk.css"
+  },
+  {
+    "revision": "166aebdeb307c4ddf3f4",
+    "url": "/random-kanye-quotes/static/js/main.3556402f.chunk.js"
+  },
+  {
+    "revision": "c74515792d3f5685f5a6",
+    "url": "/random-kanye-quotes/static/js/runtime~main.43885a69.js"
+  },
+  {
+    "revision": "fee253a53e31bc60b863",
+    "url": "/random-kanye-quotes/static/css/2.376e178f.chunk.css"
+  },
+  {
+    "revision": "fee253a53e31bc60b863",
+    "url": "/random-kanye-quotes/static/js/2.a0cf1e7f.chunk.js"
+  },
+  {
+    "revision": "5105a4d2852d4ba66acabcfc5b35bc71",
+    "url": "/random-kanye-quotes/index.html"
+  }
+];
